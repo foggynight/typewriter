e8a3a80c51b0db604964b629ecb2ee146fa43618
foggynight <foggynight709@gmail.com>
2021-01-21 11:45:06 -0330

    Add comment skeleton

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * main.c
+ * Copyright (C) 2020-2021 Robert Coffey
+ **/
+
+int main(void)
+{
+    // initialize page
+    // load file if necessary
+
+    // update loop
+
+    // write file if necessary
+
+    return 0;
+}
