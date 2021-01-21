43a93c23669b87527a10035abb5f21df52e44deb
foggynight <foggynight709@gmail.com>
2021-01-21 12:21:27 -0330

    Add page type

---
+++ inc/page.h
@@ -0,0 +1,15 @@
+/**
+ * page.h
+ * Copyright (C) 2020-2021 Robert Coffey
+ **/
+
+#ifndef PAGE_H_
+#define PAGE_H_
+
+#include <stdio.h>
+
+typedef struct page {
+    FILE *file;
+} page_t;
+
+#endif // PAGE_H_
