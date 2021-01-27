0dbe62443016eada73bf3a3c96f08d4e6ea6a9bc
foggynight <foggynight709@gmail.com>
2021-01-27 10:28:40 -0330

    Add file description

--- debug.sh
+++ debug.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# debug.sh - Build and debug led
+# Copyright (C) 2021 Robert Coffey
+# Released under the GPLv2 license
+
 gcc -g -I./inc -I./dep/rtb/inc src/*.c -o led_debug -lncurses
 gdb led_debug
 rm -f led_debug
