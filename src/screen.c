ab6bdc9f82a663504f2fd6340e86a439a7bcacf5
foggynight <foggynight709@gmail.com>
2021-02-01 01:15:00 -0330

    Draw text to center of the screen

--- src/screen.c
+++ src/screen.c
@@ -12,20 +12,30 @@
 #include "rtb/log.h"
 #include "screen.h"
 
+static int scr_width;
+static int scr_height;
+
 void screen_setup(void)
 {
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, ""); // must flush locale before ncurses init
+
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
     intrflush(stdscr, FALSE);
+
+    getmaxyx(stdscr, scr_height, scr_width); // TODO: resizable window
 }
 
 void screen_draw(cursor_t *cursor, page_t *page)
 {
+    /* draw lines of text to the screen, position of the cursor is
+     * always at the center of the screen and the text slides */
     for (int i = 0; i < page->length; ++i)
-        mvaddstr(i, 0, page->lines[i]);
+        mvaddstr(i + scr_height/2 - cursor->y_pos,
+                 scr_width/2 + cursor->x_pos,
+                 page->lines[i]);
     refresh();
 }
 
