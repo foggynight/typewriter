2222659f5ad0b5614dde2b8f4a0dcf47a6f865dc
foggynight <foggynight709@gmail.com>
2021-02-01 00:39:55 -0330

    Add cursor_t and init/destroy functions

---
+++ inc/cursor.h
@@ -0,0 +1,18 @@
+/**
+ * led - cursor.h
+ * Copyright (C) 2021 Robert Coffey
+ * Released under the GPLv2 license
+ **/
+
+#ifndef CURSOR_H_
+#define CURSOR_H_
+
+typedef struct cursor {
+    int x_pos;
+    int y_pos;
+} cursor_t;
+
+cursor_t *cursor_init(void);
+void cursor_destroy(cursor_t *target);
+
+#endif // CURSOR_H_
---
+++ src/cursor.c
@@ -0,0 +1,27 @@
+/**
+ * led - cursor.c
+ * Copyright (C) 2021 Robert Coffey
+ * Released under the GPLv2 license
+ **/
+
+#include <stdlib.h>
+
+#include "cursor.h"
+#include "rtb/log.h"
+
+cursor_t *cursor_init(void)
+{
+    cursor_t *new_cursor = calloc(1, sizeof new_cursor);
+    if (!new_cursor)
+        rtb_elog("cursor_init: calloc failed");
+
+    return new_cursor;
+}
+
+void cursor_destroy(cursor_t *target)
+{
+    if (!target)
+        rtb_elog("cursor_destroy: target is NULL");
+
+    free(target);
+}
