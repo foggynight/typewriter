b02385b47ec369416eb6d1494de96802b8debbed
foggynight <foggynight709@gmail.com>
2021-02-04 04:53:15 -0330

    Add Page class to replace page_t

---
+++ inc/Page.hpp
@@ -0,0 +1,25 @@
+/**
+ * led - page.hpp
+ * Copyright (C) 2021 Robert Coffey
+ * Released under the GPLv2 license
+ **/
+
+#ifndef PAGE_HPP_
+#define PAGE_HPP_
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Page {
+private:
+    std::string path;
+    std::fstream file;
+    std::vector<std::string> lines;
+
+public:
+    Page();
+    bool load(std::string path);
+};
+
+#endif // PAGE_HPP_
