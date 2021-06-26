bfd7373547089828fafedef74a883199a17d9acb
foggynight <foggynight709@gmail.com>
2021-06-26 02:05:14 -0230

    Add text buffer and event loop

--- typewriter.lisp
+++ typewriter.lisp
@@ -7,8 +7,39 @@
 
 (require :croatoan)
 
+(defparameter *initial-line-size* 81)
+
+(defun add-char (text-buf char)
+  )
+
+(defun del-char (text-buf)
+  )
+
+(defun draw-page (text-buf scr)
+  (crt:move scr 0 0)
+  (dolist (line text-buf)
+    (crt:add scr line))
+  (crt:refresh scr))
+
+(defun write-page-to-file (text-buf name)
+  )
+
 (defun main ()
-  (crt:with-screen (scr :input-echoing nil)
-    (ncurses:addstr "Hello, world!")
-    (crt:bind scr #\ 'exit-event-loop)
-    (crt:run-event-loop scr)))
+  (let ((text-buf `(,(format nil "This is a test.~%")
+                    ,(format nil "This is also a test.~%"))))
+    (crt:with-screen (scr :input-echoing nil)
+
+      (crt:bind scr #\esc 'exit-event-loop)
+
+      (crt:bind scr '(#\space :backspace #\tab :btab)
+                (lambda (w e)
+                  (declare (ignore w e))
+                  (draw-page text-buf scr)))
+
+      (crt:bind scr t
+                (lambda (w e)
+                  (declare (ignore w))
+                  (add-char text-buf e)
+                  (draw-page text-buf scr)))
+
+      (crt:run-event-loop scr))))
