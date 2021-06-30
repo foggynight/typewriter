7f68b073dc764d767cc411bcce3bc85d1a3ea36a
foggynight <foggynight709@gmail.com>
2021-06-30 00:42:13 -0230

    Prevent non-standard characters from being...
    ...added to the page. This also prevents unbound <CTRL> key combinations
    from writing control characters to the page.

--- typewriter.lisp
+++ typewriter.lisp
@@ -135,7 +135,7 @@
                           :process-control-chars nil)
       (screen-center-cursor scr)
 
-      ;;; -- Control Command Events --
+      ;;; -- <CTRL> Command Events --
       (crt:bind scr #\ 'crt:exit-event-loop)
       (crt:bind scr #\
                 (lambda (w e)
@@ -164,23 +164,23 @@
                   (newline cursor)
                   (screen-draw-page scr cursor page)))
 
-      ;;; -- Print Events --
-      ;; For any unbound character: add that character to the page at the cursor
-      ;; position, and move the cursor position forward.
-      (crt:bind scr t
-                (lambda (w e)
-                  (declare (ignore w))
-                  (add-char page e (y cursor) (x cursor))
-                  (move cursor :right)
-                  (screen-draw-page scr cursor page)))
-
-      ;;; -- Misc Events --
-      ;; Window resize event -- Center screen cursor and draw page
+      ;;; -- Window Events --
       (crt:bind scr :resize
                 (lambda (w e)
                   (declare (ignore w e))
                   (screen-center-cursor scr)
                   (screen-draw-page scr cursor page)))
+
+      ;;; -- Print Events --
+      ;; For any unbound character: if the character is a standard character,
+      ;; add it to the page and move the cursor, otherwise do nothing.
+      (crt:bind scr t
+                (lambda (w e)
+                  (declare (ignore w))
+                  (when (standard-char-p e)
+                    (add-char page e (y cursor) (x cursor))
+                    (move cursor :right)
+                    (screen-draw-page scr cursor page))))
 
       (screen-draw-page scr cursor page)
       (crt:run-event-loop scr))))
