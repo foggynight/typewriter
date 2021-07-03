549312ecf458ea573dfa124b5a7f3a49e0a705e1
foggynight <foggynight709@gmail.com>
2021-07-02 23:12:47 -0230

    Prevent text overflow

--- typewriter.lisp
+++ typewriter.lisp
@@ -39,6 +39,10 @@ function to map a sequence of anything to empty lines."
           do (vector-push-extend char line))
     line))
 
+(defun line-to-string (line)
+  "Convert a line to a string."
+  (coerce line 'string))
+
 ;;; CURSOR SECTION -------------------------------------------------------------
 ;;
 ;; There are two categories of cursor in this program: text and screen cursors.
@@ -159,20 +163,51 @@ named filename."
          (y (car pos)))
     (crt:move scr (1+ y) start-x)))
 
+(defun screen-draw-line (scr cursor line)
+  "Draw a line at the current position of the screen cursor, leaving the screen
+cursor unmoved."
+  (when (> (length line) 0)
+    (let* ((width (crt:width scr))
+           (win-center (crt:center-position scr))
+           (center-x (cadr win-center))
+           (first-char (- (x cursor) center-x))
+           (char-count (if (< first-char 0)
+                           (+ width first-char)
+                           width)))
+      (when (< first-char 0)
+        (crt:move-direction scr :right (- first-char))
+        (setq first-char 0))
+      (when (< first-char (length line))
+        (let ((line-to-draw (subseq line first-char)))
+          (when (< char-count (length line-to-draw))
+            (setq line-to-draw (subseq line-to-draw 0 char-count)))
+          (crt:save-excursion scr
+            (crt:add scr (line-to-string line-to-draw))))))))
+
 (defun screen-draw-page (scr cursor page)
-  "Draw a page such that the screen is centered on the text cursor."
-  (let* ((center (crt:center-position scr))
-         (center-y (car center))
-         (center-x (cadr center))
-         (start-x (- center-x (x cursor))))
+  "Draw a page with its text content positioned such that the screen is centered
+on the text cursor."
+  (let* ((height (crt:height scr))
+         (win-center (crt:center-position scr))
+         (center-y (car win-center))
+         (first-line (- (y cursor) center-y))
+         (line-count (if (< first-line 0)
+                         (+ height first-line)
+                         height)))
     (crt:save-excursion scr
       (crt:clear scr)
-      (crt:move scr
-                (- center-y (y cursor))
-                start-x)
-      (dolist (line (text-buffer page))
-        (crt:add scr line)
-        (screen-newline scr start-x)))))
+      (loop while (< first-line 0)
+            do (screen-newline scr 0)
+               (incf first-line))
+      (when (< first-line (line-count page))
+        (let ((lines-to-draw (if (> first-line 0)
+                                 (subseq (text-buffer page) first-line)
+                                 (text-buffer page))))
+          (when (< line-count (length lines-to-draw))
+            (setq lines-to-draw (subseq lines-to-draw 0 line-count)))
+          (dolist (line lines-to-draw)
+            (screen-draw-line scr cursor line)
+            (screen-newline scr 0)))))))
 
 ;;; MAIN SECTION ---------------------------------------------------------------
 
